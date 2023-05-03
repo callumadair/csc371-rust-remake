1015f385967208bad10a8ee43f0648a84ff58e9f
cal <callum.adair@outlook.com>
2023-05-03 12:18:29 +0100

    initial implementation item.rs.

--- src/item.rs
+++ src/item.rs
@@ -15,11 +15,11 @@ impl Item {
     }
 
     pub(crate) fn size(&self) -> usize {
-        return 0;
+        return self.entries.len();
     }
 
     pub(crate) fn empty(&self) -> bool {
-        return false;
+        return self.entries.is_empty();
     }
 
     pub(crate) fn get_ident(&self) -> &String {
@@ -31,30 +31,112 @@ impl Item {
     }
 
     pub(crate) fn add_entry(&mut self, key: String, value: String) -> bool {
+        if !self.entries.contains_key(&key) {
+            self.entries.insert(key, value);
+            return true;
+        }
         return false;
     }
 
-    pub(crate) fn merge_entries(&mut self, other: &mut Item) -> () {}
+    pub(crate) fn merge_entries(&mut self, other: &mut Item) -> () {
+        for (key, value) in other.entries.iter() {
+            self.add_entry(key.clone(), value.clone());
+        }
+    }
 
     pub(crate) fn get_entry(&self, key: String) -> Option<String> {
-        return None;
+        return self.entries.get(&key).cloned();
     }
 
     pub(crate) fn delete_entry(&mut self, key: String) -> bool {
+        if self.entries.contains_key(&key) {
+            self.entries.remove(&key);
+            return true;
+        }
         return false;
     }
 }
 
 impl fmt::Display for Item {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for entry in self.entries.iter() {
+            write!(f, "{}\n", serde_json::to_string(&entry).unwrap())?;
+        }
+
         return write!(f, "{}", "");
     }
 }
 
 impl PartialEq<Self> for Item {
     fn eq(&self, other: &Self) -> bool {
-        self.entries == other.entries
+        self.identifier == self.identifier && self.entries == other.entries
     }
 }
 
 impl Eq for Item {}
+
+#[cfg(test)]
+mod tests {
+    use std::string::String;
+    use super::*;
+
+    #[test]
+    fn test_empty() {
+        let identifier = String::from("Empty_Test");
+        let item = Item::new(identifier);
+        assert_eq!(item.size(), 0);
+        assert!(item.empty());
+    }
+
+    #[test]
+    fn test_entries_add() {
+        let identifier = String::from("Entries_Test");
+        let mut item = Item::new(identifier);
+
+        let first_key = String::from("url");
+        let first_val = String::from("https://www.google.com");
+
+        assert_eq!(item.add_entry(first_key.clone(), first_val.clone()), true);
+        assert_eq!(item.size(), 1);
+        assert_ne!(item.empty(), true);
+        assert_eq!(item.get_entry(String::from("url")).unwrap(), first_val);
+
+        //The add another entry with the same key.
+        assert_eq!(item.add_entry(first_key.clone(), first_val.clone()), false);
+        assert_eq!(item.size(), 1);
+        assert_ne!(item.empty(), true);
+
+        //Now try with new key value pair.
+        let second_key = String::from("username");
+        let second_val = String::from("myusername");
+
+        assert!(item.add_entry(second_key.clone(), second_val.clone()));
+        assert_eq!(item.size(), 2);
+        assert_ne!(item.empty(), true);
+        assert_eq!(item.get_entry(second_key.clone()).unwrap(), second_val);
+    }
+
+    #[test]
+    fn test_entries_delete() {
+        let identifier = String::from("Test");
+        let mut item = Item::new(identifier);
+
+        let first_key = String::from("url");
+        let first_val = String::from("https://www.google.com");
+
+        //Validate the details of the entry are correct
+        assert!(item.add_entry(first_key.clone(), first_val.clone()));
+        assert_eq!(item.size(), 1);
+        assert_ne!(item.empty(), true);
+        assert_eq!(item.get_entry(first_key.clone()).unwrap(), first_val);
+
+        //Delete non-existent entry and validate nothing changed.
+        assert_eq!(item.delete_entry(String::from("username")), false);
+        assert_eq!(item.get_entry(first_key.clone()).unwrap(), first_val);
+        assert_eq!(item.size(), 1);
+
+        //Now delete the real entry and validate it is gone.
+        assert!(item.delete_entry(first_key.clone()));
+        assert_eq!(item.size(), 0);
+    }
+}
