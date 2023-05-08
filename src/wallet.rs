6ab1cec5bb1b7c114dbb69ad765d97e0a9fe58ce
Cal <callum.adair@outlook.com>
2023-05-08 15:16:40 +0100

    Added unit tests to the wallet.rs file

--- src/wallet.rs
+++ src/wallet.rs
@@ -65,7 +65,7 @@ impl Wallet {
         for (cat_ident, category) in wallet_values.as_object().unwrap() {
             let mut new_category = Category::new(cat_ident.clone());
 
-            for (item_ident, item) in category.as_object().unwrap(){
+            for (item_ident, item) in category.as_object().unwrap() {
                 let mut new_item = new_category.new_item(item_ident);
 
                 for (entry_ident, entry_val) in item.as_object().unwrap() {
@@ -80,7 +80,9 @@ impl Wallet {
     }
 
     pub(crate) fn save(&self, filename: &String) -> bool {
-        return false;
+        let json_val = serde_json::to_string(&self).unwrap();
+        fs::write(filename, json_val).expect("Unable to write file");
+        return true;
     }
 }
 
@@ -96,3 +98,58 @@ impl PartialEq<Self> for Wallet {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty() {
+        let wallet = Wallet::new();
+        assert_eq!(wallet.size(), 0);
+        assert!(wallet.empty());
+    }
+
+    #[test]
+    fn test_categories_add() {
+        let mut wallet = Wallet::new();
+        assert!(wallet.empty());
+
+        let first_cat_ident = String::from("Test");
+        let first_category = Category::new(first_cat_ident.clone());
+        assert!(wallet.empty());
+        assert!(wallet.add_category(first_category.clone()));
+        assert_eq!(wallet.size(), 1);
+        assert!(!wallet.empty());
+        assert_eq!(wallet.get_category(&first_cat_ident) == &first_category);
+
+        let second_category = Category::new(first_cat_ident.clone());
+        assert!(second_category.empty());
+        assert!(!wallet.add_category(second_category.clone()));
+        assert_eq!(wallet.size(), 1);
+        assert!(!wallet.empty());
+
+        let third_cat_ident = String::from("Test2");
+        let third_category = Category::new(third_cat_ident.clone());
+        assert!(third_category.empty());
+        assert!(wallet.add_category(third_category.clone()));
+        assert_eq!(wallet.size(), 2);
+        assert!(!wallet.empty());
+        assert_eq!(wallet.get_category(&third_cat_ident) == &third_category);
+    }
+
+    #[test]
+    fn test_categories_delete() {
+        let mut wallet = Wallet::new();
+        assert!(wallet.empty());
+        let first_cat_ident = String::from("Test");
+        let first_category = Category::new(first_cat_ident.clone());
+        assert!(wallet.empty());
+        assert!(wallet.add_category(first_category.clone()));
+        assert_eq!(wallet.size(), 1);
+        assert!(!wallet.empty());
+        assert_eq!(wallet.get_category(&first_cat_ident) == &first_category);
+        assert!(wallet.delete_category(&first_cat_ident));
+        //add exception check here
+        assert_eq!(wallet.size(), 0);
+    }
+}
