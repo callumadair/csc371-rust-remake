b279ce5cb8adfd947782bba39da5a3c419dc70c6
Cal <callum.adair@outlook.com>
2023-05-15 19:32:33 +0100

    fixed loading entries from file to not produce quotation marks.

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,18 +1,17 @@
-use std::collections::HashMap;
-use std::{fmt, fs};
+use std::{collections, fmt, fs};
 use serde::{Serialize, Deserialize};
 use serde_json::Value;
 use crate::category::Category;
 
 #[derive(Clone, Eq, Debug, Serialize, Deserialize)]
 pub(crate) struct Wallet {
-    categories: HashMap<String, Category>,
+    categories: collections::HashMap<String, Category>,
 }
 
 impl Wallet {
     pub(crate) fn new() -> Wallet {
         Wallet {
-            categories: HashMap::new(),
+            categories: collections::HashMap::new(),
         }
     }
 
@@ -59,9 +58,8 @@ impl Wallet {
     }
 
     pub(crate) fn load(&mut self, filename: &String) -> bool {
-        let file_contents = fs::read_to_string(filename).unwrap();
+        let file_contents: String = fs::read_to_string(filename).unwrap();
         let wallet_values: Value = serde_json::from_str(&file_contents).unwrap();
-        dbg!(&wallet_values);
 
         for (cat_ident, category) in wallet_values.as_object().unwrap() {
             let mut new_category = self.new_category(cat_ident);
@@ -70,7 +68,7 @@ impl Wallet {
                 let mut new_item = new_category.new_item(item_ident);
 
                 for (entry_ident, entry_val) in item.as_object().unwrap() {
-                    new_item.add_entry(entry_ident.clone(), entry_val.to_string());
+                    new_item.add_entry(entry_ident.clone(), entry_val.as_str().unwrap().to_string());
                 }
             }
         }
@@ -157,13 +155,32 @@ mod tests {
         let file_path = String::from("./tests/testdatabasealt.json");
         assert!(Path::new(&file_path).exists());
 
-        let data = String::from("{\"Bank Accounts\":{\"Starling\":{\"Account Number\":\
-        \"12345678\",\"Name\":\"Mr John Doe\",\"Sort Code\":\"12-34-56\"}},\"Websites\":{\
-        \"Facebook\":{\"password\":\"pass1234fb\",\"url\":\"https://www.facebook.com/\",\
-        \"username\":\"example@gmail.com\"},\"Google\":{\"password\":\"pass1234\",\"url\":\
-        \"https://www.google.com/\",\"username\":\"example@gmail.com\"},\"Twitter\":{\"password\":\
-        \"r43rfsffdsfdsf\",\"url\":\"https://www.twitter.com/\",\"username\":\"example@gmail.com\"\
-        }}}");
+        let data = String::from(r#"{
+            "Bank Accounts":{
+                "Starling":{
+                    "Account Number":"12345678",
+                    "Name":"Mr John Doe",
+                    "Sort Code":"12-34-56"
+                }
+            },
+            "Websites":{
+                "Facebook":{
+                    "password":"pass1234fb",
+                    "url":"https://www.facebook.com/",
+                    "username":"example@gmail.com"
+                    },
+                "Google":{
+                    "password":"pass1234",
+                    "url":"https://www.google.com/",
+                    "username":"example@gmail.com"
+                },
+                "Twitter":{
+                    "password":"r43rfsffdsfdsf",
+                    "url":"https://www.twitter.com/",
+                    "username":"example@gmail.com"
+                    }
+                }
+            }"#);
         fs::write(&file_path, data).expect("Unable to write file");
 
         let mut wallet = Wallet::new();
